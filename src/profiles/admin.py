98765e9e79ed79feec15cd61e5d4fec26d22efa0
Denis <kopyloveden@gmail.com>
2024-05-28 22:22:22 +0500

    feat: add custom user model

--- src/profiles/admin.py
+++ src/profiles/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+@admin.register(get_user_model())
+class UserNetAdmin(UserAdmin):
+    list_display = ("username", "email", "phone", "first_name", "last_name", "is_staff")
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "middle_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+        (_("Addition info"), {"fields": ("phone", "avatar", "gender")}),
+
+    )
--- src/profiles/models.py
+++ src/profiles/models.py
@@ -4,8 +4,17 @@ from django.contrib.auth.models import AbstractUser
 
 class UserNet(AbstractUser):
     '''Custom user model'''
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+
     middle_name = models.CharField(max_length=50)
-    first_login = models.DateTimeField(null=True)
+    first_login = models.DateTimeField(blank=True, null=True)
     phone = models.CharField(max_length=14)
     avatar = models.ImageField(upload_to='user/avatar', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    github = models.CharField(max_length=500, blank=True, null=True)
+    birthday = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='M')
     

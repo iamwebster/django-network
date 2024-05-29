cb8adf642dc1a9ba770ec23db00e2a694b44eda6
Denis <kopyloveden@gmail.com>
2024-05-29 19:33:47 +0500

    feat: add jwt authorization

--- src/profiles/serializers.py
+++ src/profiles/serializers.py
@@ -8,4 +8,22 @@ class GetUserNetSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        exclude = ('password', 'last_login', 'is_active', 'is_staff', 'is_superuser')
+        exclude = ('password', 'last_login', 'is_active', 'is_staff', 'is_superuser', 'user_permissions', 'groups')
+
+
+class GetUserNetPublicSerializer(serializers.ModelSerializer):
+    '''Вывод публичной инфы по user'''
+
+    class Meta:
+        model = get_user_model()
+        exclude = (
+            'user_permissions', 
+            'groups', 
+            'email', 
+            'password', 
+            'last_login', 
+            'is_active', 
+            'is_staff', 
+            'is_superuser',
+            'phone',
+        )
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,20 +1,22 @@
-from django.contrib.auth import get_user_model 
-from .serializers import GetUserNetSerializer
+from django.contrib.auth import get_user_model
+from .serializers import GetUserNetSerializer, GetUserNetPublicSerializer
 
 from rest_framework.generics import RetrieveAPIView, UpdateAPIView
 from rest_framework import permissions
+from rest_framework.viewsets import ModelViewSet
 
 
-class GetUserNetView(RetrieveAPIView):
-    '''Вывод профиля пользователя'''
+class UserNetPublicView(ModelViewSet):
+    '''Вывод публичного профиля пользователя'''
+    serializer_class = GetUserNetPublicSerializer
+    permission_classes = [permissions.AllowAny]
     queryset = get_user_model().objects.all()
-    serializer_class = GetUserNetSerializer
 
 
-class UpdateUserNetView(UpdateAPIView):
-    '''Редактирование пользователя'''
+class UserNetView(ModelViewSet):
+    '''Вывод профиля пользователя'''
     serializer_class = GetUserNetSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        return get_user_model().objects.filter(id=self.request.user.id)
+        return get_user_model().objects.filter(pk=self.request.user.pk)
